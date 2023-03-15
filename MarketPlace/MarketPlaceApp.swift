2cc3ae6ee6159a1d6593dc2a68078f4fdf4cc303
Roma <moshkovcevroma@gmail.com>
2023-03-15 13:58:36 +0700

    Initial Commit

---
+++ MarketPlace/MarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketPlaceApp.swift
+//  MarketPlace
+//
+//  Created by Роман Мошковцев on 15.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
